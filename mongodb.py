640c3c543c5c834bb4d650c5fc0689f499b0a1f8
이현우 <ladder887@naver.com>
2024-02-19 21:19:44 +0900

    데이터 전처리 추가

--- mongodb.py
+++ mongodb.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import pandas as pd
+import re
 
 class MongoDBHandler: #몽고 디비 핸들러
     def __init__(self, host='localhost', port=27017, database_name='d3'):
@@ -11,32 +12,32 @@ class MongoDBHandler: #몽고 디비 핸들러
     def get_collection(self, collection_name): # 몽고디비에 데이터베이스를 선택하고 선택된 데이터베이스 안에 컬랙션이라는 것을 만들 수 있음 데이터베이스는 서비스 전체, 컬랙션은 유저 목록, 상품 종류 이런거임.
         return self.db[collection_name]
     
-    def insert_document(self, collection, standard):
-        print(standard.TweetID)
+    def insert_document(self, collection, standard, AccountID):
         # 기존 데이터 가져오기
-        existing_doc = collection.find_one({"AccountID": standard.AccountID})
+        existing_doc = collection.find_one({"AccountID": AccountID})
 
         if existing_doc: #기존에 데이터가 존재하는지 안하는지 확인
-            print("AccountID already exists in the database:", standard.AccountID)
+            print("AccountID already exists in the database:", AccountID)
 
             # 기존 데이터 업데이트
             update_result = collection.update_one(
-                {"AccountID": standard.AccountID},
+                {"AccountID": AccountID},
                 {
                     "$push": {
                         'TweetData': {
-                            'TweetID' : standard.TweetID,
+                            'TweetID' : id_data_preprocessing(standard.TweetID),
                             'TweetContent' : standard.TweetContent,
-                            'AuthorID' : standard.AuthorID,
-                            'OriginalTweetID' : standard.OriginalTweetID,
+                            'TweetContentPreprocessing' : content_data_processing(standard.TweetContent),
+                            'AuthorID' : id_data_preprocessing(standard.AuthorID),
+                            'OriginalTweetID' : id_data_preprocessing(standard.OriginalTweetID),
                             'TweetCreated' : standard.TweetCreated,
-                            'TweetEntities' : standard.TweetEntities,
-                            'TweetContentHashtag' : standard.TweetContentHashtag,
-                            'TweetContentURL' : standard.TweetContentURL,
-                            'TweetMention' : standard.TweetMention,
-                            'TweetMedia' : standard.TweetMedia,
-                            'TweetMediaURL' : standard.TweetMediaURL,
-                            'OriginalTweetAuthorID' : standard.OriginalTweetAuthorID,
+                            'TweetEntities' : nan_data_preprocessing(standard.TweetEntities),
+                            'TweetContentHashtag' : list_data_preprocessing(standard.TweetContentHashtag),
+                            'TweetContentURL' : list_data_preprocessing(standard.TweetContentURL),
+                            'TweetMention' : list_data_preprocessing(standard.TweetMention),
+                            'TweetMedia' : nan_data_preprocessing(standard.TweetMedia),
+                            'TweetMediaURL' : list_data_preprocessing(standard.TweetMediaURL),
+                            'OriginalTweetAuthorID' : id_data_preprocessing(standard.OriginalTweetAuthorID),
                             'TweetLanguage' : standard.TweetLanguage,
                             'TweetRetweetCount' : standard.TweetRetweetCount,
                             'TweetReplyCount' : standard.TweetReplyCount,
@@ -44,9 +45,10 @@ class MongoDBHandler: #몽고 디비 핸들러
                             'TweetQuoteCount' : standard.TweetQuoteCount,
                             'TweetBookmarkCount' : standard.TweetBookmarkCount,
                             'TweetImpressionCount' : standard.TweetImpressionCount,
-                            'TweetReferencedTweets' : standard.TweetReferencedTweets,
-                            'TweetSource' : standard.TweetSource,
-                            'TweetPlace' : standard.TweetPlace,
+                            'TweetReferencedTweets' : nan_data_preprocessing(standard.TweetReferencedTweets),
+                            'TweetSource' : nan_data_preprocessing(standard.TweetSource),
+                            'TweetPlace' : nan_data_preprocessing(standard.TweetPlace),
+                            'Include' : nan_data_preprocessing(standard.Include),
                             'TweetContextAnnotations' : standard.TweetContextAnnotations
                         }
                     }
@@ -61,38 +63,39 @@ class MongoDBHandler: #몽고 디비 핸들러
         else:
             # 기존 데이터가 없을 경우, 새로운 데이터 삽입
             data = {
-                'AccountID' : standard.AccountID,
+                'AccountID' : AccountID,
                 'AccountNickname' : standard.AccountNickname,
                 'AccountName' : standard.AccountName,
                 'AccountCreated' : standard.AccountCreated,
-                'AccountDescription' : standard.AccountDescription,
-                'AccountEntities' : standard.AccountEntities,
-                'AccountDescriptionHashtag' : standard.AccountDescriptionHashtag,
-                'AccountDescriptionURL' : standard.AccountDescriptionURL,
-                'AccountDescriptionMention' : standard.AccountDescriptionMention,
-                'AccountLocation' : standard.AccountLocation,
-                'AccountProfileImageURL' : standard.AccountProfileImageURL,
+                'AccountDescription' : nan_data_preprocessing(standard.AccountDescription),
+                'AccountDescriptionPreprocessing' : content_data_processing(standard.AccountDescription),
+                'AccountEntities' : nan_data_preprocessing(standard.AccountEntities),
+                'AccountDescriptionHashtag' : list_data_preprocessing(standard.AccountDescriptionHashtag),
+                'AccountDescriptionURL' : list_data_preprocessing(standard.AccountDescriptionURL),
+                'AccountDescriptionMention' : list_data_preprocessing(standard.AccountDescriptionMention),
+                'AccountLocation' : nan_data_preprocessing(standard.AccountLocation),
+                'AccountProfileImageURL' : nan_data_preprocessing(standard.AccountProfileImageURL),
                 'AccountFollowersCount' : standard.AccountFollowersCount,
                 'AccountFollowingCount' : standard.AccountFollowingCount,
                 'AccountTweetCount' : standard.AccountTweetCount,
                 'AccountListedCount' : standard.AccountListedCount,
                 'AccountLikeCount' : standard.AccountLikeCount,
-                'AccountURL' : standard.AccountURL,
+                'AccountURL' : nan_data_preprocessing(standard.AccountURL),
                 'AccountVerified' : standard.AccountVerified,
-                'Include' : standard.Include,
                 'TweetData' : [{
-                    'TweetID' : standard.TweetID,
+                    'TweetID' : id_data_preprocessing(standard.TweetID),
                     'TweetContent' : standard.TweetContent,
-                    'AuthorID' : standard.TweetID,
-                    'OriginalTweetID' : standard.OriginalTweetID,
+                    'TweetContentPreprocessing' : content_data_processing(standard.TweetContent),
+                    'AuthorID' : id_data_preprocessing(standard.AuthorID),
+                    'OriginalTweetID' : id_data_preprocessing(standard.OriginalTweetID),
                     'TweetCreated' : standard.TweetCreated,
-                    'TweetEntities' : standard.TweetEntities,
-                    'TweetContentHashtag' : standard.TweetContentHashtag,
-                    'TweetContentURL' : standard.TweetContentURL,
-                    'TweetMention' : standard.TweetMention,
-                    'TweetMedia' : standard.TweetMedia,
-                    'TweetMediaURL' : standard.TweetMediaURL,
-                    'OriginalTweetAuthorID' : standard.OriginalTweetAuthorID,
+                    'TweetEntities' : nan_data_preprocessing(standard.TweetEntities),
+                    'TweetContentHashtag' : list_data_preprocessing(standard.TweetContentHashtag),
+                    'TweetContentURL' : list_data_preprocessing(standard.TweetContentURL),
+                    'TweetMention' : list_data_preprocessing(standard.TweetMention),
+                    'TweetMedia' : nan_data_preprocessing(standard.TweetMedia),
+                    'TweetMediaURL' : list_data_preprocessing(standard.TweetMediaURL),
+                    'OriginalTweetAuthorID' : id_data_preprocessing(standard.OriginalTweetAuthorID),
                     'TweetLanguage' : standard.TweetLanguage,
                     'TweetRetweetCount' : standard.TweetRetweetCount,
                     'TweetReplyCount' : standard.TweetReplyCount,
@@ -100,9 +103,10 @@ class MongoDBHandler: #몽고 디비 핸들러
                     'TweetQuoteCount' : standard.TweetQuoteCount,
                     'TweetBookmarkCount' : standard.TweetBookmarkCount,
                     'TweetImpressionCount' : standard.TweetImpressionCount,
-                    'TweetReferencedTweets' : standard.TweetReferencedTweets,
-                    'TweetSource' : standard.TweetSource,
-                    'TweetPlace' : standard.TweetPlace,
+                    'TweetReferencedTweets' : nan_data_preprocessing(standard.TweetReferencedTweets),
+                    'TweetSource' : nan_data_preprocessing(standard.TweetSource),
+                    'TweetPlace' : nan_data_preprocessing(standard.TweetPlace),
+                    'Include' : nan_data_preprocessing(standard.Include),
                     'TweetContextAnnotations' : standard.TweetContextAnnotations
                 }]
             }
@@ -114,7 +118,36 @@ class MongoDBHandler: #몽고 디비 핸들러
             else:
                 print("데이터 생성 실패")
 
+#id형식 데이터 전처리
+def id_data_preprocessing(data):
+    if not pd.notna(data):
+        return ''
+    return data.strip('[ ]')
 
+#list형식 데이터 전처리
+def list_data_preprocessing(data):
+    if not pd.notna(data):
+        return []
+    return data.strip("'[ ]'").split("', '")
+
+#NaN값 전처리
+def nan_data_preprocessing(data):
+    if not pd.notna(data):
+        return "None"
+    return data
+
+#트윗내용 사용자 소개글 전처리
+def content_data_processing(data):
+    if not pd.notna(data):
+        return "None"
+    url_del = r'http[s]?://[^\s]+'
+    hashtag_del = r'#\w+'
+    data = re.sub(url_del, '', data)
+    data = re.sub(hashtag_del, '', data)
+    data = data.strip()
+    if not data:
+        return "None"
+    return data
 
 if __name__ == "__main__":
     mongo_handler = MongoDBHandler()
@@ -126,11 +159,11 @@ if __name__ == "__main__":
     df = pd.read_csv(csv_file_path, encoding='utf-8-sig')
 
     for standard in df.itertuples():
-        #temp = my_collection.find_one({'TweetData.TweetID' : standard.TweetID})
-        temp = my_collection.find_one({'TweetData': {'$elemMatch' : {'TweetID' :standard.TweetID}}})
+
+        temp = my_collection.find_one({'TweetData': {'$elemMatch' : {'TweetID' :id_data_preprocessing(standard.TweetID)}}})
         if temp:
             print("TweetID already exists in the database: ", standard.TweetID)
             continue
-        mongo_handler.insert_document(my_collection, standard)
+        mongo_handler.insert_document(my_collection, standard, id_data_preprocessing(standard.AccountID))
         count += 1
         print(count)
--- tweet_retweeted_processing.py
+++ tweet_retweeted_processing.py
@@ -1,15 +1,59 @@
 import pandas as pd
 import re
 
-#csv
-csv_file_path = 'C:\\Users\\Cysec\\Desktop\\knowledge_graph\\sample.csv'
-result_csv_file_path = 'C:\\Users\\Cysec\\Desktop\\knowledge_graph\\sampleRetweeted.csv'
+#데이터 위치
+file_path = 'C:\\Users\\Cysec\\Desktop\\knowledge_graph\\data\\'
+result_file_path = 'C:\\Users\\Cysec\\Desktop\\knowledge_graph\\data_retweeted_processing\\'
 
+
+csv_list = ["2cb.csv", 
+        "acid.csv", 
+        "Adderall.csv", 
+        "Amphetamine.csv", 
+        "benzos.csv", 
+        "bud.csv", 
+        "carts.csv", 
+        "cocaine.csv",
+        "coke.csv",
+        "DMT.csv",
+        "dmtvape.csv",
+        "drugs.csv",
+        "drugstwt.csv",
+        "drugtwt.csv",
+        "ecstacy.csv",
+        "edibles.csv",
+        "Fentanyl.csv",
+        "hash.csv",
+        "hashish.csv",
+        "heroin.csv",
+        "hydrocodone.csv",
+        "ketamine.csv",
+        "kush.csv",
+        "lsd.csv",
+        "Marijuana.csv",
+        "mdma.csv",
+        "Mescaline.csv",
+        "meth.csv",
+        "methamphetamine.csv",
+        "mushrooms.csv",
+        "nufcfanscoke.csv",
+        "pills.csv",
+        "plug.csv",
+        "psilocybin.csv",
+        "psychedelics.csv",
+        "search420Life.csv",
+        "shrooms.csv",
+        "vegasplug.csv",
+        "weed.csv",
+        "Xanax.csv",
+        "1drugtwt.csv",
+        "1mdma.csv",
+    ]
 
 #리트윗 트윗 데이터 처리함수
-def retweeted_processing(data_set):
-    df = pd.read_csv(data_set, encoding= 'utf-8')
+def retweeted_processing(csv_file_path, result_csv_file_path):
 
+    df = pd.read_csv(csv_file_path, encoding= 'utf-8')
     for row in df.itertuples():
         #데이터에 nan이 아니고 'retweeted'이 포함되어 있을시
         if not pd.isnull(row.TweetReferencedTweets):
@@ -33,4 +77,7 @@ def retweeted_processing(data_set):
     #csv 저장
     df.to_csv(result_csv_file_path, index=False, encoding='utf-8-sig')
 
-retweeted_processing(csv_file_path)
+for csv_name in csv_list:
+    csv_file_path = file_path + csv_name
+    result_csv_file_path = result_file_path + csv_name
+    retweeted_processing(csv_file_path, result_csv_file_path)
